259b4ccac2d295d11d99705672f96b2817039ad8
Aditya Sirish <aditya@saky.in>
2024-04-17 15:39:51 -0700

    update review.sh
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- review.sh
+++ review.sh
@@ -2,6 +2,7 @@ export GITTUF_DEV=1
 gittuf rsl remote pull origin
 git fetch origin feature:feature
 git checkout feature
-gittuf dev authorize --from-ref feature -k ./keys/ossna-1 main
+gittuf dev authorize --from-ref feature -k ./keys/aditya main
 git push origin refs/gittuf/attestations:refs/gittuf/attestations
+gittuf rsl remote push origin
 gh pr review --approve --body "LGTM!"
