c03c85e44418c903201e4a4ed5441d0f9a290fb8
Aditya Sirish <aditya@saky.in>
2024-04-17 16:53:38 -0700

    Set identity to the fix
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- fix-repository.sh
+++ fix-repository.sh
@@ -1,3 +1,7 @@
+git config --local user.name "Aditya Sirish"
+git config --local user.email "aditya@example.com"
+git config --local gpg.format ssh
+git config --local user.signingkey "/home/saky/lab/gittuf/ossna-demo-controller/keys/aditya"
 gittuf rsl annotate --skip -m "No yolo!" $(git rev-parse refs/gittuf/reference-state-log)
 git revert -S --no-edit HEAD
 gittuf rsl record main
